91749ac227811cb282d360a6bcb2cf3b81b5b10f
saikumarmanne681-glitch <saikumarmanne681@gmail.com>
2026-04-02 07:24:58 +0530

    Update Apps Script URL in googleSheets.ts

--- src/lib/googleSheets.ts
+++ src/lib/googleSheets.ts
@@ -17,7 +17,7 @@ const STORAGE_KEY = getEnvStorageKey("appsScriptUrl");
 
 let APPS_SCRIPT_URL =
   localStorage.getItem(STORAGE_KEY) ||
-  "https://script.google.com/macros/s/AKfycbzr21g_EYW9L2DcKgSmGuy1gQAjs0x8jhvCVVv9XiRK_Rnym8NnTKWZSbNhAIn2rElM4w/exec";
+  "https://script.google.com/macros/s/AKfycbzWRvOrdiAc2pfz7PVh7iOiUq0-o1EFA6yzyXUeX9XHY4nPLkUuOpdrArlD8UNALvQ2LA/exec";
 
 export function getAppsScriptUrl() {
   return APPS_SCRIPT_URL;
